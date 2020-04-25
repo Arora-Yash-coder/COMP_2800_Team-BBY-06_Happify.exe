e8283cc16a742c0d2dbd1e9903177488f75d1aa5
Yash Arora <yasharora9416@gmail.com>
2020-04-24 22:46:52 -0700

    Snake Minigame

--- js files/skecth.js
+++ js files/skecth.js
@@ -1,31 +1,122 @@
-let bubble;
+class Snake {
+  
+    constructor() {
+        this.body = [];
+        this.body[0] = createVector(floor(w/2), floor(h/2));
+        this.xdir = 0;
+        this.ydir = 0;
+        this.len = 0;
+    }
+    
+    setDir(x, y) {
+        this.xdir = x;
+        this.ydir = y;
+    }
+    
+    update() {
+        let head = this.body[this.body.length-1].copy();
+        this.body.shift();
+        head.x += this.xdir;
+        head.y += this.ydir;
+        this.body.push(head);
+    }
+    
+    grow() {
+        let head = this.body[this.body.length-1].copy();
+        this.len++;
+        this.body.push(head);
+    }
+    
+    endGame() {
+        let x = this.body[this.body.length-1].x;
+        let y = this.body[this.body.length-1].y;
+        if(x > w-1 || x < 0 || y > h-1 || y < 0) {
+            return true;
+        }
+        for(let i = 0; i < this.body.length-1; i++) {
+            let part = this.body[i];
+            if(part.x == x && part.y == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    eat(pos) {
+        let x = this.body[this.body.length-1].x;
+        let y = this.body[this.body.length-1].y;
+        if(x == pos.x && y == pos.y) {
+            this.grow();
+            return true;
+        }
+        return false;
+    }
+    
+    show() {
+        for(let i = 0; i < this.body.length; i++) {
+            fill(255);
+            noStroke();
+            rect(this.body[i].x, this.body[i].y, 1, 1)
+        }
+    }
+}
+
+
+
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    bubble = new Bubble(300,200,12);
-    print(bubble.x,bubble.y);
+  createCanvas(400, 400);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
 }
-class Bubble {
-    constructor(x,y,r){
-        this.x = x;
-        this.y = y;
-        this.r = r;
-    }
-    move () {
-        this.x = this.x + random(-5,5);
-        this.y = this.y + random(-5,5);
-    }
-    show (){
-        stroke(255);
-        strokeWeight(4);
-        noFill();
-        ellipse(this.x,this.y,this.r*2);
-    }
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
+  }
+
 }
 
 function draw() {
-    background(0);
-    
-    bubble.move();
-    bubble.show();
+  scale(rez);
+  background(51);
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+
+  if (snake.endGame()) {
+    print("END GAME");
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
 }
