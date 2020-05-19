0865c3336087f4d18028ee54cd1e330e89a81160
yiwen950725 <tiffany-gu@hotmail.com>
2020-05-18 20:47:37 -0700

    Added sticky nav bars -Tiffany

--- resources/static/js/flow_final.js
+++ resources/static/js/flow_final.js
@@ -15,15 +15,3 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
-
-
-$(document).ready(function () {
-                    function say_hi() {
-                        $.get("/user_profile/getProfile", function (data, status) {
-                            $("#username").html(data.result[0].username);
-                            console.log("data.username===============")
-                            console.log(data)
-                        });
-                    } say_hi()
-
-                });
--- resources/static/js/user_profile.js
+++ resources/static/js/user_profile.js
@@ -1,4 +1,27 @@
- //=====================upload==========avatar====================
+//Sticking nav bar
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("head");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
+//=====================upload==========avatar====================
     $(document).ready(function () {
 
 
