612c716f520ff7844e341451696a1392e381fc64
yiwen950725 <tiffany-gu@hotmail.com>
2020-05-19 23:05:28 -0700

    fire mode modified -Tiffany

--- resources/static/js/user_profile.js
+++ resources/static/js/user_profile.js
@@ -11,9 +11,11 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+      navbar.classList.remove("regular");
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
+      navbar.classList.add("regular");
   }
 }
 
