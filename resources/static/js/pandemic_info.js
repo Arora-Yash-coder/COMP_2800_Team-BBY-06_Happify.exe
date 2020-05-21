350606c427d26cd6de484726970da5547e76af2f
Rickson2019 <2528889836@qq.com>
2020-05-20 22:36:11 -0700

    Added Comments in sw.js as well as pandemic_info.ejs

--- resources/static/js/pandemic_info.js
+++ resources/static/js/pandemic_info.js
@@ -1,3 +1,187 @@
+//WHEN THE BUTTON IS CLICKED,
+//THE LINE GRAPH WILL SHOW
+$("#line_graph_button").click(() => {
+    $("#line_graph").show()
+    $("#bar_graph").hide()
+})
+
+//WHEN THE BUTTON IS CLICKED,
+//THE BAR GRAPH WILL SHOW
+$("#bar_graph_button").click(() => {
+    $("#bar_graph").show()
+    $("#line_graph").hide()
+})
+
+//days IS THE ARRAY TO STORE WHICH DAY IT IS IN.
+var days = []
+var infected_num = []
+async function getLineData() {
+    console.log("doing")
+    let get_data = await $.ajax({
+        type: "get",
+        url: "/parse_covid_line_data",
+
+        dataType: "json",
+        success: function (response) {
+            data = response
+            console.log(data)
+
+            for (var i in data) {
+                console.log(data[i]._id)
+                days.push(data[i]._id.substring(5))
+                console.log(data[i].count)
+                infected_num.push(data[i].count)
+            }
+            return
+
+
+        }
+    });
+}
+
+//LINE CHART
+async function drawLineGraph() {
+    new Chart(document.getElementById("line-chart"), {
+        type: 'line',
+        data: {
+            //days
+            labels: days,
+
+            datasets: [{
+                    data: infected_num,
+                    label: "COVID NEWLY CONFIRMED CASES",
+                    borderColor: "#3e95cd",
+                    fill: true
+                },
+                // {
+                // 	data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+                // 	label: "Coupons Redeemed",
+                // 	borderColor: "#3e95cd",
+                // 	fill: false
+                // }, 
+
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'CASE DISTRIBUTION'
+            }
+        }
+    });
+
+}
+
+
+//distribution IS AN OBJECT THAT WILL BE STORING 
+//SECTION RELATED DATA AND BE USED IN THE AMCHARTS 
+//MAP DISPLAY.
+var distribution = {
+    "4 Vancouver Island": null,
+    "2 Fraser": null,
+    "1 Interior": null,
+    "3 Vancouver Coastal": null,
+    "5 Northen": null,
+
+};
+//THE SECTION IS AN ARRAY
+var section = [];
+//WHILE THE NUMBER WILL BE STORED IN HERE
+var infected_num_section = [];
+
+//THE FUNCTION TO GET BAR CHART DATA.
+async function getBarData() {
+    console.log("doing")
+    let get_data = await $.ajax({
+        type: "get",
+        url: "/parse_covid_bar_data",
+
+        dataType: "json",
+        success: function (response) {
+            data = response
+            console.log(data)
+
+            for (var i in data) {
+                console.log(data[i]._id)
+                section.push(data[i]._id)
+                console.log(data[i].count)
+                infected_num_section.push(data[i].count)
+
+                if (data[i]._id == "Vancouver Island") {
+                    distribution["4 Vancouver Island"] = data[i].count
+                } else if (data[i]._id == "Fraser") {
+                    distribution["2 Fraser"] = data[i].count
+
+                } else if (data[i]._id == "Interior") {
+                    distribution["1 Interior"] = data[i].count
+
+                } else if (data[i]._id == "Vancouver Coastal") {
+                    distribution["3 Vancouver Coastal"] = data[i].count
+
+                } else {
+                    distribution["5 Northern"] = data[i].count
+
+                }
+            }
+            return
+
+
+        }
+    });
+}
+
+//THE FUNCTION TO DEPICT THE BAR CHART
+async function drawBarGraph() {
+
+    new Chart(document.getElementById("bar-chart"), {
+        type: 'polarArea',
+        data: {
+            labels: section,
+            datasets: [{
+                //THE HEADER OF THE CHART
+                label: "Cases",
+
+                backgroundColor: "rgba(179,181,198,0.6)",
+                borderColor: "rgba(227,71,43,1)",
+
+                data: infected_num_section
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                // text: 'Distribution in % of world population'
+            }
+        }
+    });
+
+}
+
+
+
+
+
+//GET LINEGRAPH DATA FIRST
+getLineData().then(() => {
+    //THEN DEPICT THE 
+    drawLineGraph()
+}).then(() => {
+    getBarData().then(() => {
+        drawBarGraph().then(drawMap())
+    })
+})
+
+
+
+
+// DRAWGRAPH()
+$(".choice_btn").click((e) => {
+    $(e.target.parentNode.parentNode).css("display", "none")
+    $(e.target.parentNode.parentNode).css("height", "0")
+    $(e.target.parentNode.parentNode).remove()
+})
+
+//DRAWS THE MAP
 async function drawMap() {
 
 
@@ -6,23 +190,23 @@ async function drawMap() {
     console.log(distribution)
     var chart = am4core.create("chartdiv", am4maps.MapChart);
 
-    // Set map definition
+    // SET MAP DEFINITION
     chart.geodataSource.url = "/static/BC-HA-map.min.json"
 
     chart.data = distribution;
-    // Set projection
+    // SET PROJECTION
     chart.projection = new am4maps.projections.Mercator();
 
-    // Create map polygon series
+    // CREATE MAP POLYGON SERIES
     var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
 
-    // Make map load polygon (like country names) data from GeoJSON
+    // MAKE MAP LOAD POLYGON (LIKE COUNTRY NAMES) DATA FROM GEOJSON
     polygonSeries.useGeodata = true;
 
-    // Configure series
+    // CONFIGURE SERIES
     var polygonTemplate = polygonSeries.mapPolygons.template;
   
-
+    //HA_Title IS THE TITLE IN THE SHP FILE.
     polygonTemplate.tooltipText = "{HA_Title}" ;
 
    
@@ -37,19 +221,14 @@ async function drawMap() {
         console.log("title")
         let a = data.HA_Title
         console.log(a)
-        // alert(distribution[a] + "cases")
+        
+        //APPENDS THE CASE INFO INTO THE HTML DIV.
         info.innerHTML = "<h3>" + distribution[a]+ "cases" +" </h3>";
-        // if (data.HA_Title) {
-        //     info.innerHTML += distribution[a];
-        //     polygonTemplate.tooltipText = "{HA_Title}" + distribution[a];
-        // }
-        // else {
-        //     info.innerHTML += "<i>No description provided.</i>"
-        // }
+    
     })
 
 
-        // Create hover state and set alternative fill color
+        // CREATE HOVER STATE AND SET ALTERNATIVE FILL COLOR
         var hs = polygonTemplate.states.create("hover");
         hs.properties.fill = am4core.color("#367B25");
 
@@ -60,4 +239,27 @@ async function drawMap() {
         // chart.zoomControl = new am4maps.ZoomControl();
 
 
+    }
+
+
+      // WHEN THE USER SCROLLS THE PAGE, EXECUTE MYFUNCTION
+      window.onscroll = function () {
+        myFunction()
+    };
+
+    // GET THE NAVBAR
+    var navbar = document.getElementById("navbar");
+
+    // GET THE OFFSET POSITION OF THE NAVBAR
+    var sticky = navbar.offsetTop;
+
+    // ADD THE STICKY CLASS TO THE NAVBAR WHEN YOU REACH ITS SCROLL POSITION. REMOVE "STICKY" WHEN YOU LEAVE THE SCROLL POSITION
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.remove("regular");
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+            navbar.classList.add("regular");
+        }
     }
